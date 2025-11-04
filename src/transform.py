328b7bed7b09d7869b55e670ff1ebf67c9cb6fa7
King-Junco <brennankimbrell0601@gmail.com>
2025-11-04 16:18:37 -0600

    image varation

---
+++ src/transform.py
@@ -0,0 +1,27 @@
+from torchvision import transforms
+
+train_transform = transforms.Compose([
+    transforms.RandomRotation(30),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+    transforms.RandomAffine(
+        degrees=0,
+        translate=(0.1, 0.1),
+        scale=(0.9, 1.1),
+    ),
+    transforms.ColorJitter(
+        brightness=0.2,
+        contrast=0.2,
+        saturation=0.2,
+        hue=0.1,
+    ),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
+])
+
+val_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
+])
